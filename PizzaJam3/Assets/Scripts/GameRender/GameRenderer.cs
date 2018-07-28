2fb70f95c69fcb4f8af3f1fe5b39f51c86b78b04
WJLIDDY <wjl36@case.edu>
2018-07-27 20:27:59 -0400

    players and more

---
+++ PizzaJam3/Assets/Scripts/GameRender/GameRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRenderer : MonoBehaviour
+{
+    GameState gs;
+    public TileRenderer tr;
+    public PlayerRenderer pr;
+    float tick_time_left;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gs = new GameState(100);
+        gs.addClearing(new IntVec2(0, 0), 10);
+        gs.tiles_[0, 0] = new HarvesterRobot(Resource.Type.WOOD);
+        tr.Setup(gs);
+        pr.addPlayer();
+
+    }
+
+    // Update is called once per frame
+    // Update is called once per frame
+    void Update()
+    {
+
+        tick_time_left -= Time.deltaTime;
+        if (tick_time_left < 0)
+        {
+            gs.process();
+            gs.tick();
+            tr.DrawState(gs);
+            tick_time_left = GameState.TICK_TIME;
+        }
+    }
+}
